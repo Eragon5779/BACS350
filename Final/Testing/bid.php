481476df228cbe4c941143a1c0de255a383b9927
Casey Burklow <eragon5779@gmail.com>
2017-11-28 14:29:21 -0700

    Modified how bids work

--- Final/Testing/bid.php
+++ Final/Testing/bid.php
@@ -3,8 +3,13 @@
         $bid = $_POST['bid'];
         $product = $_POST['id'];
         $bidder = $_SESSION['username'];
-        $hist = ($_POST['history'] != '' ? $_POST['history'].  ',' . $bidder: $_POST['history']);
-        $hist = $hist . ': ' . $bid;
+        $hist = '';
+        if ($_POST['history'] == NULL) {
+            $hist = $bidder . ':' . $bid;
+        }
+        else {
+            $hist = $_POST['history'] . ',' . $bidder . ':' . $bid;
+        }
 
         $stmt = $db->prepare('UPDATE items SET currentBid = :bid, bidHistory = :hist WHERE id = :id');
         $stmt->bindValue(':bid',$bid);
