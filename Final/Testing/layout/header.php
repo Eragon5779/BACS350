88dc13c2936a140de4b73000d074d7b15de76a6d
matisyahu22 <33072942+matisyahu22@users.noreply.github.com>
2017-11-28 14:02:16 -0700

    REMOVE BUTT

--- Final/Testing/layout/header.php
+++ Final/Testing/layout/header.php
@@ -41,7 +41,7 @@
 			<nav id="l">CATEGORIES</nav>
 			<form id="r" action="search.php" method="get">
 			<input style="margin-right:35%;" type="text" name="keyword" placeholder="search"></input>
-			<input style="width:30%;" class="butt" type="submit" value="Submit"></input>
+			<input style="width:30%;" type="submit" value="Submit"></input>
 			</form>
 			<img src="media/logo.png"></img>
 		</div>
