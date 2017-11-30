677d40bed34709f1444c1ea511afd9a8aaf0643a
Eragon5779 <eragon5779@gmail.com>
2017-11-30 14:14:24 -0700

    Commented out login script in index.php

--- Final/Testing/index.php
+++ Final/Testing/index.php
@@ -6,33 +6,33 @@ require_once('includes/config.php');
 //if( $user->is_logged_in() ){ header('Location: index.php'); exit(); }
 
 //process login form if submitted
-if(isset($_POST['submit'])){
+//if(isset($_POST['submit'])){
 
-	if (!isset($_POST['username'])) $error[] = "Please fill out all fields";
-	if (!isset($_POST['password'])) $error[] = "Please fill out all fields";
-
-	$username = $_POST['username'];
-	if ( $user->isValidUsername($username)){
-		if (!isset($_POST['password'])){
-			$error[] = 'A password must be entered';
-		}
-		$password = $_POST['password'];
-
-		if($user->login($username,$password)){
-			$_SESSION['username'] = $username;
-			header('Location: dash-2.php');
-			exit;
-
-		} else {
-			$error[] = 'Wrong username or password.';
-		}
-	}else{
-		$error[] = 'Usernames are required to be Alphanumeric, and between 3-16 characters long';
-	}
-
-
-
-}//end if submit
+//	if (!isset($_POST['username'])) $error[] = "Please fill out all fields";
+//	if (!isset($_POST['password'])) $error[] = "Please fill out all fields";
+//
+//	$username = $_POST['username'];
+//	if ( $user->isValidUsername($username)){
+//		if (!isset($_POST['password'])){
+//			$error[] = 'A password must be entered';
+//		}
+//		$password = $_POST['password'];
+///
+//		if($user->login($username,$password)){
+///			$_SESSION['username'] = $username;
+//			header('Location: dash-2.php');
+//			exit;
+//
+//		} else {
+//			$error[] = 'Wrong username or password.';
+//		}
+//	}else{
+//		$error[] = 'Usernames are required to be Alphanumeric, and between 3-16 characters long';
+//	}
+//
+//
+//
+//}///end if submit
 
 //define page title
 $title = 'YCH';
