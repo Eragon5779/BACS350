1ed1a71c2e11fe374f9aa15f750f77bf811161ee
Eragon5779 <eragon5779@gmail.com>
2017-11-30 14:13:36 -0700

    Added login to header.php

--- Final/Testing/layout/header.php
+++ Final/Testing/layout/header.php
@@ -1,4 +1,28 @@
-<!DOCTYPE html>
+
+<?php
+//Login Script
+if(isset($_POST['submit'])){
+	if (!isset($_POST['username'])) $error[] = "Please fill out all fields";
+	if (!isset($_POST['password'])) $error[] = "Please fill out all fields";
+	$username = $_POST['username'];
+	if ( $user->isValidUsername($username)){
+		if (!isset($_POST['password'])){
+			$error[] = 'A password must be entered';
+		}
+		$password = $_POST['password'];
+		if($user->login($username,$password)){
+			$_SESSION['username'] = $username;
+			header('Location: dash-2.php');
+			exit;
+		} else {
+			$error[] = 'Wrong username or password.';
+		}
+	}else{
+		$error[] = 'Usernames are required to be Alphanumeric, and between 3-16 characters long';
+	}
+}
+
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
