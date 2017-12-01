ed575cea99e4e4e68873f96f95a1f8d1cd435bb2
Casey Burklow <eragon5779@gmail.com>
2017-12-01 11:35:55 -0700

    Fixed infinite redirect

--- Final/Testing/admin.php
+++ Final/Testing/admin.php
@@ -1,7 +1,58 @@
-<?php 
-require("dash-2.php");
+<?php require("includes/config.php");
+
+$currentUser = '';
+
+if ($_GET['username'] == NULL) {
+    $currentUser = $_SESSION['username'];
+}
+else {
+    $currentUser = $_GET['username'];
+}
+
+$stmt = $db->prepare('SELECT username, email, firstName, admin FROM users where username = :username');
+$stmt->execute(array(':username' => $currentUser));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$userInfo = array('username'=>$row['username'], 'email'=>$row['email'], 'firstName'=>$row['firstName'], 'admin'=>$row['admin']);
+
+$stmt = $db->prepare('SELECT id, title FROM items where op = :op');
+$stmt->execute(array(':op' => $currentUser));
+
+if ($userInfo['admin'] != TRUE) {
+    header("Location: dash-2.php");
+}
+
+$title = $currentUser;
+require("layout/header.php");
+$currentIsLogged = ($currentUser == $_SESSION['username']);
 ?>
 
+<body style="background-color: #333;">
+
+    <h1><?php 
+    if ($currentIsLogged) {
+        echo 'Your'; 
+    } else {
+        echo $currentUser . '\'s';
+    }
+    ?> Profile</h1>
+    <br />
+    <h3><?php 
+    if ($currentIsLogged) {
+        echo 'Your';
+    } else {
+        echo $currentUser . '\'s';
+    }
+    ?> items</h3>
+    <ul>
+    <?php
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $item = array('id'=>$row['id'], 'title'=>$row['title']);
+            echo '<li><a href="product.php?id=' . $item['id'] . '">' . $item['title'] . '</a></li><br />';
+        }
+
+    ?>
+    </ul>
 
 <?php
 
