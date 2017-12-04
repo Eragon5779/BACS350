968d4133c77155f6b7910b0bfafe4f152453f7d9
Casey Burklow <eragon5779@gmail.com>
2017-12-04 11:06:14 -0700

    Created edititem.php, added tags to additem.php

--- Final/Testing/edititem.php
+++ Final/Testing/edititem.php
@@ -1,6 +1,6 @@
 <?php require("includes/config.php");
 
-$title = 'Add New Item';
+$title = 'Edit Item';
 require("layout/header.php");
 if (!$user->is_logged_in()) {
 	header("Location: index.php");
@@ -16,20 +16,13 @@ if (!$user->is_logged_in()) {
 	  
 	  Description:<br>
 	  <input type="text" name="description" placeholder="Description">
-	  <br>
-	  
-	  Reserve:<br>
-	  <input type="text" name="reserve" placeholder="Reserve">
-	  <br>
+      <br>
 
-	  Tags:<br>
+      Tags:<br>
       <input type="text" name="tags" placeholder="Tags">
-	  <br><br><br>
-	  
-	  File Upload: <br> <bR>
-	  <input type="file" name="image" id="image">
-
-	  <p>Click "Submit" to upload the file and information</p>
+      <br>
+      
+	  <p>Click "Submit" to update information</p>
 
 	  <input type="submit" value="Submit">
 
