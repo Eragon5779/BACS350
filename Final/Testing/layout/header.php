2aab99da84ed36647c0bd52c28d1bb7348eea77f
Casey Burklow <eragon5779@gmail.com>
2017-12-04 12:46:01 -0700

    Renamed button in search

--- Final/Testing/layout/header.php
+++ Final/Testing/layout/header.php
@@ -78,7 +78,7 @@ if(isset($_POST['submit'])){
 					
 			<form id="r" action="search.php" method="get">
 			<input style="margin-right:30%;" type="text" name="keyword" placeholder="search"></input>
-			<input id="sub" style="width:27.5%;" type="submit" value="Submit"></input>
+			<input id="sub" style="width:27.5%;" type="submit" name="submit2" value="Submit"></input>
 			</form>
 			<img alt="site logo" src="media/logo.png"></img>
 		</div>
