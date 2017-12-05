eb8b4908219cb92f96d2d327ccfa54eb5feb0f51
Casey Burklow <burk0683@bears.unco.edu>
2017-12-04 23:29:58 -0700

    Fixed logic for updating user

--- Final/Testing/updateuser.php
+++ Final/Testing/updateuser.php
@@ -94,69 +94,75 @@
             }
         }
         else {
-            $title = 'Update User: ' . $_POST['username'];
-            // Get current user information
-            $stmt = $db->prepare('SELECT username, passSalt, passHash, email, firstName, lastName FROM users where username = :username');
-            $stmt->execute(array(':username' => $_POST['username']));
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            $userInfo = array('username'=>$row['username'],'passSalt'=> $row['passSalt'],'passHash'=>$row['passHash'], 'email'=>$row['email'], 'firstName'=>$row['firstName'], 'lastName'=>$row['lastName']);
-
-            // Set updated information appropriately
-            $newFirstName = $_POST['firstName'];
-            $newLastName = $_POST['lastName'];
-            $newEmail = $_POST['email'];
-
-            // If any information isn't updated, let it be the old information
-            if ($newFirstName == NULL) {
-                $newFirstName = $userInfo['firstName'];
-            }
-            if ($newLastName == NULL) {
-                $newLastName = $userInfo['lastName'];
-            }
-            if ($newEmail == NULL) {
-                $newEmail = $userInfo['email'];
-            }
-
-            if ($_POST['oldPass'] != NULL || $_POST['newPass'] != NULL || $_POST['confirmPass'] != NULL) {
-                if ($_POST['oldPass'] == NULL || $_POST['newPass'] == NULL || $_POST['confirmPass'] == NULL) {
-                    $filled = FALSE;
+            if (isset($_POST['firstName']) || isset($_POST['lastName']) || isset($_POST['email']) ||isset($_POST['oldPass']) ||isset($_POST['newPass']) ||isset($_POST['confirmPass']))) {
+                $title = 'Update User: ' . $_POST['username'];
+                // Get current user information
+                $stmt = $db->prepare('SELECT username, passSalt, passHash, email, firstName, lastName FROM users where username = :username');
+                $stmt->execute(array(':username' => $_POST['username']));
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                $userInfo = array('username'=>$row['username'],'passSalt'=> $row['passSalt'],'passHash'=>$row['passHash'], 'email'=>$row['email'], 'firstName'=>$row['firstName'], 'lastName'=>$row['lastName']);
+    
+                // Set updated information appropriately
+                $newFirstName = $_POST['firstName'];
+                $newLastName = $_POST['lastName'];
+                $newEmail = $_POST['email'];
+    
+                // If any information isn't updated, let it be the old information
+                if ($newFirstName == NULL) {
+                    $newFirstName = $userInfo['firstName'];
                 }
-                else if (!password_verify($_POST['oldPass'], $userInfo['passHash'])) {
-                    $validated = FALSE;
+                if ($newLastName == NULL) {
+                    $newLastName = $userInfo['lastName'];
                 }
-                else if ($_POST['oldPass'] == $_POST['newPass']) {
-                    $matched = TRUE;
+                if ($newEmail == NULL) {
+                    $newEmail = $userInfo['email'];
                 }
-                else if ($_POST['newPass'] != $_POST['confirmPass']) {
-                    $confirmed = FALSE;
+    
+                if ($_POST['oldPass'] != NULL || $_POST['newPass'] != NULL || $_POST['confirmPass'] != NULL) {
+                    if ($_POST['oldPass'] == NULL || $_POST['newPass'] == NULL || $_POST['confirmPass'] == NULL) {
+                        $filled = FALSE;
+                    }
+                    else if (!password_verify($_POST['oldPass'], $userInfo['passHash'])) {
+                        $validated = FALSE;
+                    }
+                    else if ($_POST['oldPass'] == $_POST['newPass']) {
+                        $matched = TRUE;
+                    }
+                    else if ($_POST['newPass'] != $_POST['confirmPass']) {
+                        $confirmed = FALSE;
+                    }
+                    else {
+                        $options = [
+                            'cost' => 12,
+                            'salt' => $userInfo['passSalt']
+                        ];
+                        $newPassHash = $user->password_hash($_POST['newPass'], PASSWORD_BCRYPT, $options);
+                        $stmt = $db->prepare('UPDATE users SET passHash=:passHash, firstName=:firstName, lastName=:lastName, email=:email WHERE username=:username');
+                        $stmt->execute(array(
+                            ':passHash' => $newPassHash,
+                            ':firstName' => $newFirstName,
+                            ':lastName' => $newLastName,
+                            ':email' => $newEmail,
+                            ':username' => $_POST['username']
+                        ));
+                        $updated = TRUE;
+                    }
                 }
                 else {
-                    $options = [
-                        'cost' => 12,
-                        'salt' => $userInfo['passSalt']
-                    ];
-                    $newPassHash = $user->password_hash($_POST['newPass'], PASSWORD_BCRYPT, $options);
-                    $stmt = $db->prepare('UPDATE users SET passHash=:passHash, firstName=:firstName, lastName=:lastName, email=:email WHERE username=:username');
+                    $stmt = $db->prepare('UPDATE users SET firstName=:firstName, lastName=:lastName, email=:email WHERE username=:username');
                     $stmt->execute(array(
-                        ':passHash' => $newPassHash,
                         ':firstName' => $newFirstName,
                         ':lastName' => $newLastName,
                         ':email' => $newEmail,
-                        ':username' => $_POST['username']
+                        ':username' => $_SESSION['username']
                     ));
                     $updated = TRUE;
                 }
             }
             else {
-                $stmt = $db->prepare('UPDATE users SET firstName=:firstName, lastName=:lastName, email=:email WHERE username=:username');
-                $stmt->execute(array(
-                    ':firstName' => $newFirstName,
-                    ':lastName' => $newLastName,
-                    ':email' => $newEmail,
-                    ':username' => $_SESSION['username']
-                ));
-                $updated = TRUE;
+                $title = 'Update User: ' . $_POST['username'];
             }
+            
         }
     }
     
