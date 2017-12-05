67d73b1281955a98263c2e9c818830c1d4e0079a
Casey Burklow <burk0683@bears.unco.edu>
2017-12-04 17:33:42 -0700

    Added logic to deleteitem for user deleting own item

--- Final/Testing/deleteitem.php
+++ Final/Testing/deleteitem.php
@@ -5,11 +5,17 @@ $stmt->execute(array(':username' => $_SESSION['username']));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 $userInfo = array('admin'=>$row['admin']);
 
-if (!$userInfo['admin']) {
+$id = $_POST['id3'];
+
+$stmt = $db->prepare('SELECT op FROM items where id = :id');
+$stmt->execute(array(':id'=>$id));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$itemInfo = array('op'=>$row['op']);
+
+if (!$userInfo['admin'] || $_SESSION['username'] != $itemInfo['op']) {
     header("Location: index.php");
 }
 
-$id = $_POST['id3'];
 
 $stmt = $db->prepare('DELETE FROM items where id = :id');
 $stmt->execute(array(':id' => $id));
